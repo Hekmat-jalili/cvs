f020913bf8d40e243755816a38bf5b5adcc34b83
Hekmat-jalili <93056599+Hekmat-jalili@users.noreply.github.com>
2021-10-24 06:19:53 +0430

    added initial cv website files

---
+++ index.js
@@ -0,0 +1,113 @@
+function changeHeading(){
+    
+    document.getElementById("heading").innerHTML="<h1>welcome to Hekmat Developer Website</h1>";
+    document.getAnimations(ResizeObserverSize());
+
+}
+
+function All(){ 
+    var innerHeight = 199;
+
+     document.getElementById("first").src="works/1.png";
+    //  document.getElementById("first").animate=innerHeight;
+     document.getElementById("second").src="works/2.png";
+     document.getElementById("third").src="works/3.png";
+     document.getElementById("four").src="works/4.png";
+     document.getElementById("five").src="works/5.png";
+     document.getElementById("six").src="works/6.png";
+     document.getElementById("seven").src="works/7.png";
+     document.getElementById("eight").src="works/8.png";
+     document.getElementById("nine").src="works/9.png";
+     document.getElementById("ten").src="works/10.png";
+     document.getElementById("eleven").src="works/big.png";
+
+
+
+
+    }
+
+    function web(){
+        document.getElementById("first").src="";
+        document.getElementById("second").src="works/2.png";
+        document.getElementById("third").src="works/3.png";
+        document.getElementById("four").src="works/4.png";
+        document.getElementById("five").src="works/5.png";
+        document.getElementById("six").src="works/6.png";
+        document.getElementById("seven").src="works/7.png";
+        document.getElementById("nine").src="";
+        document.getElementById("ten").src="";
+        document.getElementById("eleven").src="";
+
+    }
+
+    function print(){
+     
+        document.getElementById("first").src="works/10.png"
+        document.getElementById("second").src="works/2.png";
+        document.getElementById("third").src="works/3.png";
+        document.getElementById("four").src="works/4.png";
+        document.getElementById("five").src="works/5.png";
+        document.getElementById("six").src="works/6.png";
+        document.getElementById("seven").src="";
+        document.getElementById("nine").src="";
+        document.getElementById("ten").src="";
+        document.getElementById("eleven").src="";
+
+    }
+
+    function photos(){
+     
+        document.getElementById("first").src="works/10.png"
+        document.getElementById("second").src="works/2.png";
+        document.getElementById("third").src="works/3.png";
+        document.getElementById("four").src="works/4.png";
+        document.getElementById("five").src="";
+        document.getElementById("six").src="";
+        document.getElementById("seven").src="";
+        document.getElementById("nine").src="";
+        document.getElementById("ten").src="";
+        document.getElementById("eleven").src="";
+
+    }
+
+
+
+    function animationbetweenImages(){
+        document.getElementById("image1").style.marginLeft="200px";
+        document.getElementById("image4").style.marginRight="300px";
+    }
+
+    function animations(){
+
+        document.getElementById("image1").style.marginLeft="";
+        document.getElementById("image4").style.marginRight="";
+    }
+
+
+    // this is the function which change the background color for the new post button
+
+
+    function ChangeBackground(){
+        document.getElementById("buttons").style.backgroundColor="teal";
+
+    
+    }
+
+    function animationForChangeBackgroundButton(){
+        document.getElementById("buttons").style.backgroundColor="orange";
+    }
+
+
+    document.getElementById("img1").style.borderRadius="150px";
+    document.getElementById("img2").style.borderRadius="150px";
+    document.getElementById("img3").style.borderRadius="150px";
+    document.getElementById("img4").style.borderRadius="150px";
+    document.getElementById("img5").style.borderRadius="150px";
+
+
+    
+    // document.getElementById("img1").style.width="200px";
+    // document.getElementById("img2").style.width="200px";
+    // document.getElementById("img3").style.width="200px";
+    // document.getElementById("img4").style.width="200px";
+    // document.getElementById("img5").style.width="0px";
